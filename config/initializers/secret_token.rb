98d06a47793ada30f6b43cb46a22522a808f1b68
Rai butera <rai@mmg.io>
2014-04-02 14:41:26 +0100

    Bundle fix

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'dcfe217cae70ad7955f158cddd806b6e2e777e512d1e4040e9f8090afabfdb731767ff99b919d8114e7dbb644872f3a363ff4bdd44bae64ccbb6af84efb1c8e9'
+SampleApp::Application.config.secret_token = '65ea1476374b0f1bc21c381ccf683d02963c522257d1351bd4382b395437b1d57a60d80523a77e289df0f6121259a73ed560cc7bc823f9b0a1cb787b94312662'
