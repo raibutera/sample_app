729c40d69a1987178e5ce7f0e2c276aa703b0dc1
Rai Butera <rai@rbutera.com>
2014-04-04 15:19:24 +0100

    user validates name length max 50

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,9 +25,14 @@ describe User do
     before { @user.name = " " }
     it {should_not be_valid}
   end
-  
+
   describe "when email is not present" do
     before { @user.email = " " }
     it {should_not be_valid}
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
   end
 end
