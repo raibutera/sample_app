14558904a145270a6d79bac1fedb12111e5d4aff
Rai Butera <rai@rbutera.com>
2014-04-08 18:48:41 +0100

    implemented successful signup form submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # handle successful save
+      # on successful save redirect to user's profile
+      redirect_to @user
     else
       render 'new'
     end
