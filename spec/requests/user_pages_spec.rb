154579f3910a3aca1362871f342b6502622c2ee8
Rai Butera <rai@rbutera.com>
2014-04-08 19:04:44 +0100

    Added tests for error messages in signup form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,6 +32,16 @@ describe "User pages" do
         it "should not create a user" do
           expect { click_button submit }.not_to change(User, :count)
         end
+
+        describe "after submission" do
+          before { click_button submit }
+
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content('error') }
+          
+          it { should have_selector('div', class: 'alert-error') }
+          it { should have_selector('li') }
+        end
       end
 
       describe "with valid information" do
