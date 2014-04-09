e679df4b7f116495c2a1b3e9c40a44b78af30156
Rai Butera <rai@rbutera.com>
2014-04-09 21:44:58 +0100

    implemented store_location when checking if the user is signed in and utilised redirect_back_or on successful sign-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       # sign in user
       sign_in user
-      redirect_to user
+      redirect_back_or(user)
     else
       # throw errors
       flash.now[:error] = 'Invalid email/password combination'
