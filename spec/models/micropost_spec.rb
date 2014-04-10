e78c92b42e10a59c0accc6f80829d30849c90ea7
Rai Butera <rai@rbutera.com>
2014-04-10 15:24:28 +0100

    added test for validity of new micropost

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -12,5 +12,12 @@ describe Micropost do
   subject { @micropost }
 
   it { should respond_to(:content) }  
-  it { should respond_to(:user_id) }  
+  it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+      before { @micropost.user_id = nil }
+      it { should_not be_valid }
+    end  
 end
