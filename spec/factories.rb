848407c7a0fe5a1ffea8a4a36efb6b95d7e90345
Rai Butera <rai@rbutera.com>
2014-04-10 10:49:19 +0100

    added tests for pagination, updated user index view for pagination, and paginate users in the index action

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name  "Hippo Potamus"
-    email "hippo@potamus.io"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@examp.le" }
     password "foobar"
     password_confirmation "foobar"
   end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -103,20 +103,30 @@ describe "User pages" do
     end
   end
 
+  
   describe "index" do
-    before do
-      sign_in FactoryGirl.create(:user)
-      FactoryGirl.create(:user, name: "Nate", email: "natan@test.io")
-      FactoryGirl.create(:user, name: "Vic", email: "victor@test.io")
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    before(:each) do
+      sign_in user
       visit users_path
     end
 
-    it {should have_title('All users')}
-    it {should have_heading('All users')}
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1',    text: 'All users') }
 
-    it "should list each user" do
-      User.all.each do |user|
-        page.should have_selector('li', text: user.name)
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.name)
+        end
       end
     end
   end
