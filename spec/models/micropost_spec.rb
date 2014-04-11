0c6b298291d2546b32d3a718b5670eadb69b91d3
Rai Butera <rai@rbutera.com>
2014-04-11 10:51:33 +0100

    added tests for micropost user assn

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -13,7 +13,9 @@ describe Micropost do
 
   it { should respond_to(:content) }  
   it { should respond_to(:user_id) }
-
+  it { should respond_to(:user) }
+  its(:user) { should == user }
+  
   it { should be_valid }
 
   describe "when user_id is not present" do
