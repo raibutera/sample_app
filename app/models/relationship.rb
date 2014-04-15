7911dcb49db4889778868abb639aecdb6955147a
Rai Butera <rai@rbutera.com>
2014-04-15 18:08:12 +0100

    added relationship model validations

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,7 @@ class Relationship < ActiveRecord::Base
 
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+  validates :follower, presence: true
+  validates :followed, presence: true
 end
