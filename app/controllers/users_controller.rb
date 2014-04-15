a6bbffb7904bfdcb7fe1e1fbde98777a5a5e26b5
Rai Butera <rai@rbutera.com>
2014-04-15 19:41:04 +0100

    added following and followers actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,20 @@ class UsersController < ApplicationController
     end 
   end
 
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def following
+    @title = "following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
 
     def correct_user
