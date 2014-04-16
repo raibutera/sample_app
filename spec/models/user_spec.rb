7c4f220e81a071752bbec2a816ff2f1f92e4e1a4
Rai Butera <rai@rbutera.com>
2014-04-16 11:10:47 +0100

    final tests for status feed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -156,7 +156,7 @@ describe User do
   end
 
 
-  describe "micropost assocations" do
+  describe "micropost associations" do
 
     before do
       @user.save
@@ -177,10 +177,21 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum")}
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
 
     it "should destroy associated microposts" do
