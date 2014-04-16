f081c32a7f4e6ed916d24bcac340479350c5d330
Rai Butera <rai@rbutera.com>
2014-04-16 10:59:23 +0100

    implemented follow/ufollow ajax request

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,18 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user } # create.html.erb
+      format.js                         # create.js.erb
+    end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user } # destroy.html.erb
+      format.js                         # destroy.js.erb
+    end
   end
 end
